9ed8f73e37a22493bed688cc1d3cf5475015c988
chenkai <1490520478@qq.com>
2019-01-10 15:55:56 +0800

    Ble_GeteWay V0.8

---
+++ 删除编译产生的文件.bat
@@ -0,0 +1,9 @@
+
+del /Q MDK-ARM\build\*.*
+del /Q MDK-ARM\DebugConfig\*.*
+del /Q MDK-ARM\*.bin
+del /Q MDK-ARM\*.uvguix.Administrator
+rd /Q /S MDK-ARM\build
+rd /Q /S MDK-ARM\DebugConfig
+
+pause
